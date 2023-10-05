dcff06278dfadc45c192eaf387bf8890d1c68aeb
Georgi GeorgIev <joretivac@gmail.com>
2023-10-05 13:57:40 +0300

    make cube create work wirh db, removal of uniqid

--- Cubicle-Workshop/src/controllers/cubeController.js
+++ Cubicle-Workshop/src/controllers/cubeController.js
@@ -4,9 +4,9 @@ router.get("/create", (req, res) => {
   res.render("create");
 });
 
-router.post("/create", (req, res) => {
+router.post("/create", async (req, res) => {
   const { name, description, imageUrl, difficultyLevel } = req.body;
-  cubeServices.create({ name, description, imageUrl, difficultyLevel });
+  await cubeServices.create({ name, description, imageUrl, difficultyLevel });
   res.redirect("/");
 });
 
--- Cubicle-Workshop/src/services/cubeService.js
+++ Cubicle-Workshop/src/services/cubeService.js
@@ -1,49 +1,9 @@
-const uniqid = require("uniqid");
+const Cube = require("../models/Cube");
+const cubes = [];
+exports.create = async (cubeData) => {
+  const cube = await Cube.create(cubeData);
 
-const cubes = [
-  {
-    id: "18e5gpdt0ln4mf0cj",
-    name: "Test1",
-    description: "Test1",
-    imageUrl:
-      "https://ae01.alicdn.com/kf/HTB1CSddXRxRMKJjy0Fdq6yifFXa6/Gan-356-Air-SM-3x3-Black-Magic-cube-GAN-Air-SM-Magnetic-3x3x3-Speed-cube-gans.jpg",
-    difficultyLevel: "1",
-  },
-  {
-    id: "18e5gpdt0ln4mf4ra",
-    name: "Test2",
-    description: "Test2",
-    imageUrl:
-      "https://ae01.alicdn.com/kf/HTB1CSddXRxRMKJjy0Fdq6yifFXa6/Gan-356-Air-SM-3x3-Black-Magic-cube-GAN-Air-SM-Magnetic-3x3x3-Speed-cube-gans.jpg",
-    difficultyLevel: "6",
-  },
-  {
-    id: "18e5gpdt0ln4mf8jt",
-    name: "Test3",
-    description: "Test3",
-    imageUrl:
-      "https://ae01.alicdn.com/kf/HTB1CSddXRxRMKJjy0Fdq6yifFXa6/Gan-356-Air-SM-3x3-Black-Magic-cube-GAN-Air-SM-Magnetic-3x3x3-Speed-cube-gans.jpg",
-    difficultyLevel: "4",
-  },
-  {
-    id: "18e5gpdt0ln4mfe0x",
-    name: "Test4",
-    description: "Test4",
-    imageUrl:
-      "https://ae01.alicdn.com/kf/HTB1CSddXRxRMKJjy0Fdq6yifFXa6/Gan-356-Air-SM-3x3-Black-Magic-cube-GAN-Air-SM-Magnetic-3x3x3-Speed-cube-gans.jpg",
-    difficultyLevel: "2",
-  },
-];
-
-exports.create = (cubeData) => {
-  const newCube = {
-    id: uniqid(),
-    ...cubeData,
-  };
-
-  cubes.push(newCube);
-
-  return newCube;
+  return cube;
 };
 
 exports.getAll = (search, from, to) => {
