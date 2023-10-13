adc242201dbcfef6c94b0328b493999b2bcbfa04
Georgi GeorgIev <joretivac@gmail.com>
2023-10-13 17:05:34 +0300

    hash password before save

--- Cubicle-Workshop/src/models/User.js
+++ Cubicle-Workshop/src/models/User.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
 
 const userSchema = new mongoose.Schema({
   username: String,
@@ -10,6 +11,11 @@ userSchema.virtual("repeatPassword").set(function (value) {
     console.log("Password missmatch");
     // throw new mongoose.MongooseError("Passowrd missmatch");
   }
+});
+
+userSchema.pre("save", async function () {
+  const hash = await bcrypt.hash(this.password, 10);
+  this.password = hash;
 });
 const User = mongoose.model("User", userSchema);
 
